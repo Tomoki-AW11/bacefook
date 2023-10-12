de4a6a43fa41ed0e53119d8cb194aeb872041f15
Tomoki-AW11 <133087164+Tomoki-AW11@users.noreply.github.com>
2023-10-12 12:48:00 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+let login = 0;
 window.addEventListener("load", () => {
   // This is a check to see if there's a username stored
   let username = localStorage.getItem("username");
@@ -5,7 +6,12 @@ window.addEventListener("load", () => {
     // Prompt for one if a username isn't found
     username = window.prompt("What is your name?");
     localStorage.setItem("username", username);
+    login = 1;
   }
+  const usernameEl = document.querySelector("#username");
+  const addUsername = document.createElement("div");
+  addUsername.innerText = localStorage.username;
+  usernameEl.append(addUsername);
   readPost();
 })
 
@@ -28,31 +34,36 @@ function onclickSample() {
   readPost()
 }
 
-
-function formClick(){
-  let userName = document.getElementById("username").value
-  let textArea = document.getElementById("userpost").value
-  let formSelect = document.getElementById("feeling").value
-  const formPost = {
-    friend: userName,
-    text: textArea,
-    feeling: formSelect,
-    image: getRandomElement(images),
-    timestamp: new Date()
-  };
-  bacefook.newsfeed.push(formPost);
-
-  document.getElementById("userpost").value = "";
-  document.getElementById("feeling").value = "";
-  onclickSample();
+function formClick() {
+  
+  if(login === 1){
+    let userName = document.getElementById("username")
+    userName = userName.firstElementChild.innerText;
+    let textArea = document.getElementById("userpost").value
+    let formSelect = document.getElementById("feeling").value
+    const formPost = {
+      friend: userName,
+      text: textArea,
+      feeling: formSelect,
+      image: getRandomElement(images),
+      timestamp: new Date()
+    };
+    bacefook.newsfeed.push(formPost);
+  
+    document.getElementById("userpost").value = "";
+    document.getElementById("feeling").value = "";
+    onclickSample();
+  }else{
+    window.alert('登録名を入力するべし！！');
+  }
 }
 
-function readPost(){
+function readPost() {
   console.log("read!")
   const containerEl = document.querySelector("#newsfeed");
   for (let index = bacefook.newsfeed.length - 1; index >= 0; index--) {
     const post = bacefook.newsfeed[index];
-  
+
     const friendEl = document.createElement("div");
     friendEl.className = "friend";
     friendEl.innerText = post.friend;
@@ -72,30 +83,46 @@ function readPost(){
     imgEl.height = 200; // 縦サイズ（px）
     postEl.append(imgEl); // 指定した要素にimg要素を挿入
     const timeStampEl = document.createElement("div");
-    
+
     let postDate = moment(post.timestamp, "YYYYMMDD");
     const nowDate = moment(new Date(), "YYYYMMDD");
     const fomPostDate = postDate.format('YYYY年MM月DD日 HH:mm:ss dddd');
     const elpTime = nowDate.diff(postDate, "HH:mm");
-  
-    if(elpTime > 604800000){
+
+    if (elpTime > 604800000) {
       timeStampEl.innerText = fomPostDate
-    }else if(elpTime > 86400000){
+    } else if (elpTime > 86400000) {
       timeStampEl.innerText = Math.floor((elpTime / 86400000)) + "日前の投稿"
-    }else if(elpTime > 3600000){
+    } else if (elpTime > 3600000) {
       timeStampEl.innerText = Math.floor((elpTime / 3600000)) + "時間前の投稿"
-    }else if(elpTime > 60000){
+    } else if (elpTime > 60000) {
       timeStampEl.innerText = Math.floor((elpTime / 60000)) + "分前の投稿"
-    }else if(elpTime > 10000){
+    } else if (elpTime > 10000) {
       timeStampEl.innerText = Math.floor((elpTime / 1000)) + "秒前の投稿"
-    }else{
+    } else {
       timeStampEl.innerText = "たった今の投稿"
     }
-  
+
     postEl.append(timeStampEl);
     containerEl.append(postEl);
   }
 }
 
+function buttonClick() {
+  localStorage.clear();
+  const containerEl = document.querySelector("#newsfeed");
+  const usernameEl = document.querySelector("#username");
+  usernameEl.textContent = "";
+  containerEl.textContent = "";
+  login = 0;
+  readPost()
+}
+
+const scheduler = () => {
+    setTimeout(scheduler, 5000); 
+    onclickSample();
+};
+
+scheduler();
 
 ;
